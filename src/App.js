7391dfe67f4fd8dc11273e0949091c4a06bb162f
AlexNines <p39q10m4@gmail.com>
2020-08-07 19:12:39 +0400

    base functional fo todo

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import TodoList from './Todo/TodoList'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const todos = [
+        {id: 1, completed: false, title: 'Buy the bread'},
+        {id: 2, completed: false, title: 'Buy the fruits'},
+        {id: 3, completed: false, title: 'Buy the meat'},
+        {id: 4, completed: false, title: 'Buy the beer'}
+
+
+
+    ]
+  return (<div className='wrapper'>
+      <h1>React app</h1>
+
+      <TodoList todos={todos} />
+  </div>)
+   
 }
 
 export default App;
