f291cc3fdf29a289858b48c361a348666eb45db3
AlexNines <p39q10m4@gmail.com>
2020-08-11 21:49:23 +0400

    create hook useInputValue

--- src/App.js
+++ src/App.js
@@ -1,47 +1,53 @@
-import React from "react"
+import React, { useEffect } from "react"
 import TodoList from "./Todo/TodoList"
 import Context from './context'
-import AddTodo from "./Todo/AddTodo";
+import AddTodo from "./Todo/AddTodo"
 
 function App() {
-    const [todos, setTodos] = React.useState([
-    { id: 1, completed: false, title: ")  Buy the bread" },
-    { id: 2, completed: false, title: ")  Buy the fruits" },
-    { id: 3, completed: false, title: ")  Buy the meat" },
-    { id: 4, completed: false, title: ")  Buy the beer" },
-  ]);
+    const [todos, setTodos] = React.useState([]);
 
-  function toggleTodo(id) {
-      setTodos(todos.map(todo => {
-          if (todo.id === id) {
-              todo.completed = !todo.completed
-          }
-          return todo
-      }))
-  }
+    useEffect(() => {
+        fetch('https://jsonplaceholder.typicode.com/todos?_limit=5')
+          .then(response => response.json())
+          .then(todos => {
+            setTimeout(() => {
+              setTodos(todos)
+              
+            }, 2000)
+          })
+      }, [])
 
-function removeTodo(id){
-    setTodos(todos.filter(todo => todo.id !==id ))
-}
+    function toggleTodo(id) {
+        setTodos(todos.map(todo => {
+            if (todo.id === id) {
+                todo.completed = !todo.completed
+            }
+            return todo
+        }))
+    }
 
-function addTodo(title) {
-    setTodos(todos.concat([{
-        title,
-        id: Date.now,
-        completed: false
-    }]))
-}
+    function removeTodo(id) {
+        setTodos(todos.filter(todo => todo.id !== id))
+    }
+
+    function addTodo(title) {
+        setTodos(todos.concat([{
+            title,
+            id: Date.now,
+            completed: false
+        }]))
+    }
 
     return (
-        <Context.Provider value = {{ removeTodo }}>
-    <div className="wrapper">
-      <h1>React app</h1>
-      <AddTodo onCreate={addTodo}/>
-{todos.length ? <TodoList todos={todos}  onToggle = {toggleTodo} /> : <p>No todos</p> }
-      
-    </div>
-    </Context.Provider>
-  )
+        <Context.Provider value={{ removeTodo }}>
+            <div className="wrapper">
+                <h1>React app</h1>
+                <AddTodo onCreate={addTodo} />
+                {todos.length ? <TodoList todos={todos} onToggle={toggleTodo} /> : <p>No todos</p>}
+
+            </div>
+        </Context.Provider>
+    )
 }
 
 export default App
