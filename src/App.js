96dbdc2ae69c99be73fe43cbe569c52999d86f62
AlexNines <p39q10m4@gmail.com>
2020-08-12 17:58:42 +0400

    lazy loading for AddTodo

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React, { useEffect } from "react"
 import TodoList from "./Todo/TodoList"
 import Context from './context'
-import AddTodo from "./Todo/AddTodo"
 import Loader from './Loader'
+
+const AddTodo = React.lazy(() => import('./Todo/AddTodo'))
 
 function App() {
     const [todos, setTodos] = React.useState([])
@@ -45,7 +46,10 @@ function App() {
         <Context.Provider value={{ removeTodo }}>
             <div className="wrapper">
                 <h1>React app</h1>
+                <React.Suspense fallback={<Loader />}>
                 <AddTodo onCreate={addTodo} />
+                </React.Suspense>
+                
                 {loading && <Loader />}
                 {todos.length ? <TodoList todos={todos} onToggle={toggleTodo} /> : <p>No todos</p>}
 
